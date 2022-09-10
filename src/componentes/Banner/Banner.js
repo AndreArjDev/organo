a30147a1cb1da3f19027c5ae88088efe43957a41
andre.araujo <andre.araujo@dbserver.com.br>
2022-09-10 09:54:32 -0300

    add componentes

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner(){
+
+    return (
+        <header className='banner'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9aapocmflE2zXkPTHd3LQh3tTHQ5HrtRM8w&usqp=CAU" alt='cabeçalho Organograma'/>
+        </header>
+    )
+
+}
+
+export default Banner;
