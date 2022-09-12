bca513587d914d68b8c85a45111adae3bbc11fbe
andre <andre.021arj@gmail.com>
2022-09-11 21:03:32 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -9,8 +9,7 @@ function App() {
 
   const aoNovoColaborador = (colaborador) => {
       setColaboradores([...colaboradores,colaborador])
-      console.log(colaboradores)
-      
+            
   }
   
   return (
